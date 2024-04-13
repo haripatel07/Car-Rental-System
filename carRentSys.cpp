e35a642fda8a7d9f4e671213930d78f82f874537
DEVang0876 <23aiml014@charusat.edu.in>
2024-04-13 12:13:35 +0530

    STarteed

--- carRentSys.cpp
+++ carRentSys.cpp
@@ -2,8 +2,38 @@
 // 23AIML014_DEVANG
 // 23AIML010_OM
 // 23AIML  ANSH
+
 #include<iostream>
+#include<iomanip>
+#include<cstring>
+
 using namespace std;
+
+//declaration of classes
 int main();
-class Car;
-//DEVICE CHECK DONE
+class USER
+{
+    string username;
+    string password;
+};
+class driver
+{
+    string d_name;
+    int age;
+    long int licence_num, cont_num, adhar;
+    public:
+};
+class car: public driver
+{  
+    string car_name;
+    long int vehicle_num;
+    public:
+};
+
+int main()
+{
+    cout<<endl<<"-------------WELCOME TO CAR RENTAL PLATFORM-------------";
+    cout<<endl<<"BOOK A CAB ?";
+    cout<<endl<<"WANT TO BE A TEXI DRIVER ?";
+    return 0;
+}
